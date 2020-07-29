13290d6490d4283aeade03c1f5450d4ab15d99b4
fpowder <fpowder1@gmail.com>
2020-07-29 21:54:21 +0900

    add unit test

--- src/main/java/com/example/prac1/restservice/GreetingController.java
+++ src/main/java/com/example/prac1/restservice/GreetingController.java
@@ -1,6 +1,7 @@
 package com.example.prac1.restservice;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,6 +18,11 @@ public class GreetingController {
 
         return new Greeting(counter.incrementAndGet(), String.format(template, name));
 
+    }
+
+    @RequestMapping("/")
+    public String index(){
+        return "Greetings from Spring Boot!";
     }
 
 }
--- src/test/java/com/example/prac1/Prac1ApplicationTests.java
+++ src/test/java/com/example/prac1/Prac1ApplicationTests.java
@@ -1,13 +1,36 @@
 package com.example.prac1;
 
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 
 @SpringBootTest
+@AutoConfigureMockMvc
 class Prac1ApplicationTests {
 
-    @Test
+    @Autowired
+    private MockMvc mvc;
+
+    /*@Test
     void contextLoads() {
+
+    }*/
+
+    @Test
+    public void getHello() throws Exception{
+        String greetStr = "Greetings from Spring Boot!";
+        mvc.perform(MockMvcRequestBuilders.get("/").accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(MockMvcResultMatchers.content().string("Greetings from Spring Boot!"));
     }
 
 }
